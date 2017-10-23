0cbb54cd57d1d528989cef51c72855982112a77c
klaskoski <konrad.laskoski@e-podroznik.pl>
2017-10-23 23:04:26 +0200

    spring security started

---
+++ src/main/java/com/training/fullstack/common/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.training.fullstack.common.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_METHODS = {
+            "/webjars/**",
+            "/css/**",
+            "/js/**",
+            "/",
+            "/about/**",
+            "/contact/**",
+            "/error/**/*",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC_METHODS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER");
+    }
+}
