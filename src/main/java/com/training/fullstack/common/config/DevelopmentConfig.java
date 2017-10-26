54131fc5d50c800018a9055c54de842be1548a53
klaskoski <konrad.laskoski@mail.com>
2017-10-26 22:37:01 +0200

    added configuration for h2 console

--- src/main/java/com/training/fullstack/common/config/DevelopmentConfig.java
+++ src/main/java/com/training/fullstack/common/config/DevelopmentConfig.java
@@ -2,6 +2,8 @@ package com.training.fullstack.common.config;
 
 import com.training.fullstack.backend.service.EmailService;
 import com.training.fullstack.backend.service.MockEmailService;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,5 +17,12 @@ public class DevelopmentConfig {
     @Bean
     EmailService emailService(){
         return new MockEmailService();
+    }
+
+    @Bean
+    public ServletRegistrationBean h2ConsoleRegistration(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new WebServlet());
+        bean.addUrlMappings("/console/*");
+        return bean;
     }
 }
--- src/main/java/com/training/fullstack/common/config/SecurityConfig.java
+++ src/main/java/com/training/fullstack/common/config/SecurityConfig.java
@@ -2,13 +2,20 @@ package com.training.fullstack.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
 
     private static final String[] PUBLIC_METHODS = {
             "/webjars/**",
@@ -18,10 +25,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/about/**",
             "/contact/**",
             "/error/**/*",
+            "/console/**"
     };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains("dev")){
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC_METHODS).permitAll()
                 .anyRequest().authenticated()
